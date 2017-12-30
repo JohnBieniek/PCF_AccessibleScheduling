1cc3f68f0cd3b8fcd740314681fcc9688fd4851f
John Bieniek <Johanthan_A_Bieniek@homedepot.com>
2017-12-29 20:43:51 -0500

    Added CleaningController and the ability to detect/remove orphans from the browser

---
+++ src/main/java/org/cloudfoundry/samples/music/web/CleaningController.java
@@ -0,0 +1,61 @@
+package org.cloudfoundry.samples.music.web;
+
+import java.util.ArrayList;
+
+import org.cloudfoundry.samples.music.managers.CleaningManager;
+import org.cloudfoundry.samples.music.managers.CustomDataManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import accessiblesolutions.accessiblescheduling.domain.CustomFieldData;
+import accessiblesolutions.accessiblescheduling.domain.RecurringShiftNeed;
+import accessiblesolutions.accessiblescheduling.domain.ShiftRequest;
+
+@RestController
+@RequestMapping(value = "/cleaning")
+public class CleaningController {
+    @Autowired
+    private CustomDataManager customDataManager;
+    @Autowired
+    private CleaningManager cleaningManager;
+    public CleaningController() {
+    }
+    @RequestMapping(value = "/orphanedCustomFieldData", method = RequestMethod.GET)
+    public ArrayList<CustomFieldData> getOrphanedCustomFieldData() {
+        return customDataManager.getOrphanedCustomFieldData();
+    }
+    @RequestMapping(value = "/orphanedShiftRequests", method = RequestMethod.GET)
+    public ArrayList<ShiftRequest> getOrphanedShiftRequests() {
+        return cleaningManager.getOrphanedShiftRequests();
+    }
+    @RequestMapping(value = "/orphanedRecurringShiftRequests", method = RequestMethod.GET)
+    public ArrayList<RecurringShiftNeed> getOrphanedRecurringShiftRequests() {
+        return cleaningManager.getOrphanedRecurringShiftRequests();
+    }
+    @RequestMapping(value = "/removeOrphanedCustomFieldData", method = RequestMethod.GET)
+    public void removeOrphanedCustomFieldData() {
+        customDataManager.removeOrphanedCustomFieldData();
+    }
+    @RequestMapping(value = "/removeOrphanedShiftRequests", method = RequestMethod.GET)
+    public void removeOrphanedShiftRequests() {
+        cleaningManager.removeOrphanedSingleShiftRequests();
+    }
+    @RequestMapping(value = "/removeOrphanedRecurringShiftRequests", method = RequestMethod.GET)
+    public void removeOrphanedRecurringShiftRequests() {
+       cleaningManager.removeOrphanedRecurringShiftRequests();
+    }
+    @RequestMapping(value = "/orphanedCustomFieldData", method = RequestMethod.DELETE)
+    public void deleteOrphanedCustomFieldData() {
+        customDataManager.removeOrphanedCustomFieldData();
+    }
+    @RequestMapping(value = "/orphanedShiftRequests", method = RequestMethod.DELETE)
+    public void deleteOrphanedShiftRequests() {
+        cleaningManager.removeOrphanedSingleShiftRequests();
+    }
+    @RequestMapping(value = "/orphanedRecurringShiftRequests", method = RequestMethod.DELETE)
+    public void deleteOrphanedRecurringShiftRequests() {
+       cleaningManager.removeOrphanedRecurringShiftRequests();
+    }
+}
