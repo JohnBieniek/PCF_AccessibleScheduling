30823825590df2eab46e8345d3fb875d8a2da0bf
JohnBean <1126076+JohnBean@users.noreply.github.com>
2019-10-16 21:31:20 -0400

    created initial new login button

--- src/main/resources/static/js/page/loginPage.js
+++ src/main/resources/static/js/page/loginPage.js
@@ -25,47 +25,47 @@ function LoginController($scope, $modal, $http) {
 	
 	function onSignIn(googleUser) {
 	    // The ID token you need to pass to your backend:
-	    var id_token = googleUser.getAuthResponse().id_token;
-	    $scope.setIdToken(id_token);
-	    $scope.$apply();
-	    
-	    var xhr = new XMLHttpRequest();
-	    xhr.open('POST', 'https://accessiblescheduling-dev.cfapps.io/auth/tokensignin');
-//	    xhr.open('POST', 'https://accessiblescheduling.cfapps.io/auth/tokensignin');
-	    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	    xhr.onload = function() {
-	        $scope.setProfile(JSON.parse(xhr.responseText));
-	        $scope.setUser($scope.profile.user);
-	        $scope.setManager($scope.profile.manager);
-	        $scope.setAdmin($scope.profile.admin);
-		    $scope.$apply();
-		    if($scope.profile.user){
-			    $scope.setPage('employee');
-			    $scope.$apply();
-		    }
-		    else{
-		    	$http({
-		            url: '/auth/signedup',
-		            method: 'GET',
-		            headers: {
-		                'Content-Type': 'application/x-www-form-urlencoded'
-		            },
-		            params: {
-		            	idtoken:$scope.idToken
-		            }
-		        })
-		        .then(function(response) {
-		        	if(response.data=="true"){
-		        		$scope.setPage('awaitingAccess');
-		        	}
-		        	else{
-					    $scope.setPage('signUp');		        		
-		        	}
-		        	$scope.$apply();
-		        });
-		    }
-	    };
-	    xhr.send('idtoken=' + id_token);
+//	    var id_token = googleUser.getAuthResponse().id_token;
+//	    $scope.setIdToken(id_token);
+//	    $scope.$apply();
+//	    
+//	    var xhr = new XMLHttpRequest();
+//	    xhr.open('POST', 'https://accessiblescheduling-dev.cfapps.io/auth/tokensignin');
+////	    xhr.open('POST', 'https://accessiblescheduling.cfapps.io/auth/tokensignin');
+//	    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//	    xhr.onload = function() {
+//	        $scope.setProfile(JSON.parse(xhr.responseText));
+//	        $scope.setUser($scope.profile.user);
+//	        $scope.setManager($scope.profile.manager);
+//	        $scope.setAdmin($scope.profile.admin);
+//		    $scope.$apply();
+//		    if($scope.profile.user){
+//			    $scope.setPage('employee');
+//			    $scope.$apply();
+//		    }
+//		    else{
+//		    	$http({
+//		            url: '/auth/signedup',
+//		            method: 'GET',
+//		            headers: {
+//		                'Content-Type': 'application/x-www-form-urlencoded'
+//		            },
+//		            params: {
+//		            	idtoken:$scope.idToken
+//		            }
+//		        })
+//		        .then(function(response) {
+//		        	if(response.data=="true"){
+//		        		$scope.setPage('awaitingAccess');
+//		        	}
+//		        	else{
+//					    $scope.setPage('signUp');		        		
+//		        	}
+//		        	$scope.$apply();
+//		        });
+//		    }
+//	    };
+//	    xhr.send('idtoken=' + id_token);
 	  }
 	window.onSignIn = onSignIn;
 }
